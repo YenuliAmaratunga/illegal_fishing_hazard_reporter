6cc2e3d13e188da00906a066058b7adfa98e36e9
YenuliAmaratunga <yenulibeemansi@gmail.com>
2025-09-30 17:49:00 +0530

    BE server.js and app.js updated

--- backend/gps-reporting-service/src/server.js
+++ backend/gps-reporting-service/src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`GPS Reporting Service is running on port ${PORT}`);
 });
 

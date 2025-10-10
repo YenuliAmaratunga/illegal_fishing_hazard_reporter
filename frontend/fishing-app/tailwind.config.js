5a133f8ec2dddb9b3979f85cd51f4840ee8dd57e
Dhiyanah <dheeliyaudeen@gmail.com>
2025-10-10 22:04:05 +0530

    added the missing hash

--- frontend/fishing-app/tailwind.config.js
+++ frontend/fishing-app/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
         blue: "#3C467B", 
         blueLight: "#50589C", 
-        darkBlue: "000435",
+        darkBlue: "#000435",
         darkPurple: "#636CCB",
         purple: "#6E8CFB",
         lightPurple: "#D8D8FF",

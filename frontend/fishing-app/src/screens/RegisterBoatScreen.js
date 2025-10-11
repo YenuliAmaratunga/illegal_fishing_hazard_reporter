2a450d471eb5f56f125337d001f7baadb5cdf79c
SachiniGayathma <sachinigayathma@yahoo.com>
2025-10-11 12:07:09 +0530

    Edited The Boat & Trip Registration Pages

--- frontend/fishing-app/src/screens/RegisterBoatScreen.js
+++ frontend/fishing-app/src/screens/RegisterBoatScreen.js
@@ -126,7 +126,7 @@ export default function RegisterBoatScreen() {
         });
       });
 
-      await axios.post(`${AUTH_BASE}/registerBoat`, formData, {
+      await axios.post(`${AUTH_BASE}/api/Boat/registerBoat`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
           Authorization: `Bearer ${token}`,

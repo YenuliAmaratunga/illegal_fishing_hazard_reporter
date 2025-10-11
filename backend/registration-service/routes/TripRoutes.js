8b29d6b0d0aa5bd3aea0f1f878cf009251121868
Dhiyanah <dheeliyaudeen@gmail.com>
2025-10-11 22:12:37 +0530

    added the fetch latest trip endpoint

--- backend/registration-service/routes/TripRoutes.js
+++ backend/registration-service/routes/TripRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const route = express.Router();
-const {registerTrip} = require('../controllers/registerTrip'); 
+const {registerTrip, getLatestTrip} = require('../controllers/registerTrip'); 
 const {authMiddleware}= require('../Authmiddleware/authMiddleware')
 
 
 route.post('/registerTrip', authMiddleware, registerTrip);
+route.get('/latestTrip', authMiddleware, getLatestTrip);
 
 module.exports = route;
